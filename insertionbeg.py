dc312e4451d38fee61f26f4b934a41473a97bfa6
deepanraj-lgtm <deepanraj20007@gmail.com>
2026-07-21 15:59:02 +0530

    Add files via upload

---
+++ insertionbeg.py
@@ -0,0 +1,29 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+def display(head):
+    temp = head
+    while temp is not None:
+        print(temp.data,end="->")
+        temp = temp.next
+    print("None")
+n1 = Node(10)
+n2 = Node(20)
+n3 = Node(30)
+n1.next=n2
+n2.next=n3
+print("Orginal Linked List")
+head=n1
+display(head)
+
+
+print("\nInsertion at begining")
+new_node=Node(5)
+new_node.next = head
+head = new_node
+display(head)print("\nInsertion at begining")
+new_node=Node(5)
+new_node.next = head
+head = new_node
+display(head)
