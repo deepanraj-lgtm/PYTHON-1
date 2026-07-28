e4a4c86a76ef2a35b6c27f20e5ac4bc2d3eda8d3
deepanraj-lgtm <deepanraj20007@gmail.com>
2026-07-28 15:45:32 +0530

    Add files via upload

---
+++ deletionmid.py
@@ -0,0 +1,26 @@
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.next = None
+def display(head):
+    temp = head
+    while temp is not None:
+        print(temp.data,end="->")
+        temp = temp.next
+    print("None")
+n1 = Node(10)
+n2 = Node(20)
+n3 = Node(30)
+n1.next=n2
+n2.next=n3
+
+print("Orginal Linked List")
+head=n1
+display(head)
+
+print("\nDeletion in the middle")
+temp=head
+while temp.next.data!=25:
+    temp=temp.next
+temp.next=temp.next.next
+display(head)
